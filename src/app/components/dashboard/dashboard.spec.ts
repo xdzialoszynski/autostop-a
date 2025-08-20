a56c4e950908c207b2897157a3e6e6171fcb8e0d
Dzialoszynski <xdzialoszynski@gmail.com>
2025-08-20 16:56:37 +0200

    fix(Dashboard): mock GeocodingService pour les tests

--- src/app/components/dashboard/dashboard.spec.ts
+++ src/app/components/dashboard/dashboard.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Dashboard } from './dashboard';
+import { of } from 'rxjs';
+import { GeocodingService } from '../../services/geocoding/geocoding-service';
 
 describe('Dashboard', () => {
   let component: Dashboard;
   let fixture: ComponentFixture<Dashboard>;
 
   beforeEach(async () => {
+    const geocodingServiceMock = {
+      getAutocompletion: () => of([])
+    };
+
     await TestBed.configureTestingModule({
-    imports: [Dashboard]
-})
-    .compileComponents();
+      imports: [Dashboard],
+      providers: [{ provide: GeocodingService, useValue: geocodingServiceMock }]
+    })
+      .compileComponents();
 
     fixture = TestBed.createComponent(Dashboard);
     component = fixture.componentInstance;
