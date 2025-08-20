4a7ed8d848074ccbdc3b3353c9ebdcf156961cf4
Dzialoszynski <xdzialoszynski@gmail.com>
2025-08-20 16:49:26 +0200

    fix(geocoding-service): fournit le client http et mock les requetes http

--- src/app/services/geocoding/geocoding-service.spec.ts
+++ src/app/services/geocoding/geocoding-service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { GeocodingService } from './geocoding-service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('GeocodingService', () => {
   let service: GeocodingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [provideHttpClientTesting(), provideHttpClient()] });
     service = TestBed.inject(GeocodingService);
   });
 
