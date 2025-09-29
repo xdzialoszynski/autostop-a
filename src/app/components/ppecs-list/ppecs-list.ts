ec86e9fbe0368d288e8d985735bf9c5134e7c898
Dzialoszynski <xdzialoszynski@gmail.com>
2025-09-29 17:57:18 +0200

    feat(ppecs-list): implement PpecsList component with dynamic item rendering and styling

--- src/app/components/ppecs-list/ppecs-list.ts
+++ src/app/components/ppecs-list/ppecs-list.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, signal, Signal } from '@angular/core';
+import { AppStateService } from '../../services/app-state-service';
+import { Ppec } from '../../models/ppec-interface';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-ppecs-list',
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './ppecs-list.html',
   styleUrl: './ppecs-list.scss'
 })
 export class PpecsList {
+  items: Signal<Ppec[]> = signal([]);;
 
+  constructor(private state: AppStateService) {
+    this.items = this.state.ppecs;
+  }
 }
