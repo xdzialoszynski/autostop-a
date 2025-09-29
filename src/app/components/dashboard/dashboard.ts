3bb3042e42a141c48fe0f4a5c907cdc42b20066f
Dzialoszynski <xdzialoszynski@gmail.com>
2025-09-29 15:43:29 +0200

    feat(dashboard): restructure layout and integrate PpecsList component

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { StatusBarComponent } from '../status-bar/status-bar';
 import { AutostopMap } from '../autostop-map/autostop-map';
+import { PpecsList } from '../ppecs-list/ppecs-list';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
-  imports: [StatusBarComponent, AutostopMap]
+  imports: [StatusBarComponent, AutostopMap, PpecsList]
 })
 export class Dashboard {
 
