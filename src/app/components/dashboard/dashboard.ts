738e0940dae048565ff13bfecc94cea0eee19a60
Dzialoszynski <xdzialoszynski@gmail.com>
2025-10-01 11:44:41 +0200

    chore(ppec-list):show list if request is sent

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,15 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { StatusBarComponent } from '../status-bar/status-bar';
+import { Observable } from 'rxjs';
+import { AppStateService } from '../../services/app-state-service';
 import { AutostopMap } from '../autostop-map/autostop-map';
 import { PpecsList } from '../ppecs-list/ppecs-list';
+import { StatusBarComponent } from '../status-bar/status-bar';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
-  imports: [StatusBarComponent, AutostopMap, PpecsList]
+  imports: [StatusBarComponent, AutostopMap, PpecsList, CommonModule]
 })
 export class Dashboard {
+  constructor(private state: AppStateService) { }
+
+  shouldShowPpecsList(): Observable<boolean> {
+    return this.state.requestSent$;
+  }
 
 }
